090d3c25d448b44eeca907633ada21b6e1adf7da
Jorge Calmon Neto <jorgecmb@Mac-mini-de-Jorge-Neto.local>
2016-07-08 06:33:13 -0400

    Views and DAOs

--- src/main/java/org/casadocodigo/loja/daos/ProdutoDAO.java
+++ src/main/java/org/casadocodigo/loja/daos/ProdutoDAO.java
@@ -5,8 +5,10 @@ import javax.persistence.PersistenceContext;
 
 import org.casadocodigo.loja.models.Produto;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class ProdutoDAO {
 	
 	@PersistenceContext
