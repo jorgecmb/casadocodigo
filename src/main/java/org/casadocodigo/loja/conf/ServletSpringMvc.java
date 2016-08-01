aa2f00418a975741e2485603ed67966798434009
Jorge Calmon Neto <jorgecmb@Mac-mini-de-Jorge-Neto.local>
2016-08-01 10:27:02 -0400

    Adding Spring Security

--- src/main/java/org/casadocodigo/loja/conf/ServletSpringMvc.java
+++ src/main/java/org/casadocodigo/loja/conf/ServletSpringMvc.java
@@ -11,7 +11,7 @@ public class ServletSpringMvc extends AbstractAnnotationConfigDispatcherServletI
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return null;
+        return new Class[] {SecurityConfiguration.class};
     }
 
     @Override
